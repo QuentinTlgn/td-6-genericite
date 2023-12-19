42b487d22d02ab7ab66a948036af65080088a23d
QuentinTlgn <tauleigq@3il.fr>
2023-12-19 14:54:54 +0100

    fini j'crois

---
+++ src/exo3p2/Exo3p2.java
@@ -0,0 +1,19 @@
+package exo3p2;
+
+import java.util.Random;
+
+public class Exo3p2<T> {
+    public Exo3p2() {
+    }
+
+
+    public static <T> T random(T obj1, T obj2){
+        Random rnd = new Random();
+        T res = obj1;
+
+        if(rnd.nextBoolean())
+            res = obj1;
+        return res;
+        
+    }
+}
